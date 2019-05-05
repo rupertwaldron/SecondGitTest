f6352ae732c534a71aefb82473617a35e427315e
rupertwaldron <rupert.waldron@yahoo.co.uk>
2019-05-05 14:06:48 +0100

    Feature 1 added.

--- src/com/ruppyrup/Main.java
+++ src/com/ruppyrup/Main.java
@@ -3,6 +3,10 @@ package com.ruppyrup;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(feature1());
+    }
+
+    public static String feature1() {
+        retur "Text for feature 1."
     }
 }
